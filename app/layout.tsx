aa9fee3885412cc880b707a565bbb5badac89c2f
coderz-aksain <ayushnikhil2002@gmail.com>
2024-03-03 23:21:55 +0530

    this is a commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,21 @@
+import { Metadata } from 'next';
+import { ThemeProvider } from "./theme";
+import { ThemeSwitcher } from "./ThemeSwitcher";
+// import { switchThemeDuration } from "../constants";
 import '@/app/ui/global.css';
+
 import { inter } from '@/app/ui/fonts';
  
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -10,5 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
+
   );
 }
